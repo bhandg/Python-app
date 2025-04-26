1a6d937481c1deadae12769018c640cae5691bac
bhandg <103158731+bhandg@users.noreply.github.com>
2025-04-26 18:55:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Dockerized World! Welcome to Webhook"
+    return "Hello, Dockerized World! Welcome to Webhook well"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
