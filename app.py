89b8ae576bb815209d2f6eb01fad70ab26fd4765
bhandg <103158731+bhandg@users.noreply.github.com>
2025-06-08 10:42:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Dockerized World! github hits the jenkins url"
+    return "Hello, Dockerized World! github hits the jenkins url & trigger cicd"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
